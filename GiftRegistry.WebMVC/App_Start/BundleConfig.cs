497cf86b3cb2adf90e379cfc637f6f4c6e5fb00c
Zach Nichols <znichols1131@gmail.com>
2022-02-04 11:15:34 -0500

    Moved most styling to Site.css (except for view-specific items), added custom button styles that match website color scheme

--- GiftRegistry.WebMVC/App_Start/BundleConfig.cs
+++ GiftRegistry.WebMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GiftRegistry.WebMVC
 
             bundles.Add(new Bundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
