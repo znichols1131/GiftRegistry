b4a45901be6479fdde57ef49665ae77ef950f9a8
Zach Nichols <znichols1131@gmail.com>
2022-02-17 12:46:54 -0500

    post-deployment

---
+++ GiftRegistry.Data/Migrations/202202170131060_init.cs
@@ -0,0 +1,223 @@
+namespace GiftRegistry.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Friend",
+                c => new
+                    {
+                        FriendID = c.Int(nullable: false, identity: true),
+                        OwnerGUID = c.Guid(nullable: false),
+                        Relationship = c.String(),
+                        PersonID = c.Int(nullable: false),
+                        IsPending = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.FriendID)
+                .ForeignKey("dbo.Person", t => t.PersonID, cascadeDelete: true)
+                .Index(t => t.PersonID);
+            
+            CreateTable(
+                "dbo.Person",
+                c => new
+                    {
+                        PersonID = c.Int(nullable: false, identity: true),
+                        PersonGUID = c.Guid(nullable: false),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        Birthdate = c.DateTime(),
+                        ProfilePicture = c.Binary(),
+                    })
+                .PrimaryKey(t => t.PersonID);
+            
+            CreateTable(
+                "dbo.Transaction",
+                c => new
+                    {
+                        TransactionID = c.Int(nullable: false, identity: true),
+                        DateCreated = c.DateTime(nullable: false),
+                        DateModified = c.DateTime(),
+                        QtyGiven = c.Int(nullable: false),
+                        GiftID = c.Int(nullable: false),
+                        GiverID = c.Int(),
+                    })
+                .PrimaryKey(t => t.TransactionID)
+                .ForeignKey("dbo.Gift", t => t.GiftID, cascadeDelete: true)
+                .ForeignKey("dbo.Person", t => t.GiverID)
+                .Index(t => t.GiftID)
+                .Index(t => t.GiverID);
+            
+            CreateTable(
+                "dbo.Gift",
+                c => new
+                    {
+                        GiftID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Description = c.String(maxLength: 100),
+                        SourceURL = c.String(),
+                        QtyDesired = c.Int(nullable: false),
+                        WishListID = c.Int(nullable: false),
+                        ProductImage = c.Binary(),
+                    })
+                .PrimaryKey(t => t.GiftID)
+                .ForeignKey("dbo.WishList", t => t.WishListID, cascadeDelete: true)
+                .Index(t => t.WishListID);
+            
+            CreateTable(
+                "dbo.WishList",
+                c => new
+                    {
+                        WishListID = c.Int(nullable: false, identity: true),
+                        OwnerGUID = c.Guid(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 100),
+                        DueDate = c.DateTime(),
+                        OwnerID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.WishListID)
+                .ForeignKey("dbo.Person", t => t.OwnerID, cascadeDelete: true)
+                .Index(t => t.OwnerID);
+            
+            CreateTable(
+                "dbo.CustomImage",
+                c => new
+                    {
+                        ImageID = c.Int(nullable: false, identity: true),
+                        OwnerGUID = c.Guid(nullable: false),
+                        ImageData = c.Binary(nullable: false),
+                    })
+                .PrimaryKey(t => t.ImageID);
+            
+            CreateTable(
+                "dbo.Notification",
+                c => new
+                    {
+                        NotificationID = c.Int(nullable: false, identity: true),
+                        NotificationType = c.Int(nullable: false),
+                        Message = c.String(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                        DateUpdated = c.DateTime(),
+                        RecipientID = c.Int(nullable: false),
+                        SenderID = c.Int(),
+                    })
+                .PrimaryKey(t => t.NotificationID)
+                .ForeignKey("dbo.Person", t => t.RecipientID, cascadeDelete: true)
+                .ForeignKey("dbo.Person", t => t.SenderID)
+                .Index(t => t.RecipientID)
+                .Index(t => t.SenderID);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.Notification", "SenderID", "dbo.Person");
+            DropForeignKey("dbo.Notification", "RecipientID", "dbo.Person");
+            DropForeignKey("dbo.Friend", "PersonID", "dbo.Person");
+            DropForeignKey("dbo.Transaction", "GiverID", "dbo.Person");
+            DropForeignKey("dbo.Transaction", "GiftID", "dbo.Gift");
+            DropForeignKey("dbo.Gift", "WishListID", "dbo.WishList");
+            DropForeignKey("dbo.WishList", "OwnerID", "dbo.Person");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.Notification", new[] { "SenderID" });
+            DropIndex("dbo.Notification", new[] { "RecipientID" });
+            DropIndex("dbo.WishList", new[] { "OwnerID" });
+            DropIndex("dbo.Gift", new[] { "WishListID" });
+            DropIndex("dbo.Transaction", new[] { "GiverID" });
+            DropIndex("dbo.Transaction", new[] { "GiftID" });
+            DropIndex("dbo.Friend", new[] { "PersonID" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.Notification");
+            DropTable("dbo.CustomImage");
+            DropTable("dbo.WishList");
+            DropTable("dbo.Gift");
+            DropTable("dbo.Transaction");
+            DropTable("dbo.Person");
+            DropTable("dbo.Friend");
+        }
+    }
+}
